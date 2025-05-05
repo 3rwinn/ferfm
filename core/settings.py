42103da295a9e987f12725dec7e0a999f2b5aefc
3rwinn <ismaelgrebo@gmail.com>
2025-05-05 12:34:30 +0000

    fix: add droplet url

--- core/settings.py
+++ core/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://207.154.226.57"]
-
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "207.154.226.57"]
 
 # Application definition
 
