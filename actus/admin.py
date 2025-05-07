6e5dd0943ab0e74f19aea29cd02ea5e300311072
3rwinn <ismaelgrebo@gmail.com>
2025-05-07 11:46:18 +0000

    feat: admin setup for actu model

--- actus/admin.py
+++ actus/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Actu
 
-# Register your models here.
+@admin.register(Actu)
+class ActuAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_short_text', 'created_at')
+    search_fields = ('text',)
+    list_filter = ('created_at',)
+    readonly_fields = ('created_at',)
+
+    def get_short_text(self, obj):
+        return obj.text[:75] + '...' if len(obj.text) > 75 else obj.text
+    get_short_text.short_description = 'Text (Shortened)'
