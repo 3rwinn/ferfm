710db7fdd71d5461e113185d4fb58110e18914ce
3rwinn <ismaelgrebo@gmail.com>
2025-05-07 14:49:47 +0000

    update prompt

--- services/gemini_service.py
+++ services/gemini_service.py
@@ -12,6 +12,16 @@ def generate_answer(user_question, knowledge_snippets):
     prompt = f"""
     INSTRUCTIONS: You are a helpful virtual assistant providing support in FRENCH. Base your answers solely on the provided knowledge base.
     Always refer to the knowledge base as "your knowledge" don't call it documents or anything else.
+    
+    Avoid using the word "documents" or "document" or "connaissances" or "connaisance" in your response
+    Also avoid those expressions:
+    - "selon mes connaissances"
+    - "selon les documents"
+    - "selon le document"
+
+    RULES:
+    - If you don't have information about the question, just say "I don't know" and don't make up an answer.
+    - If you have the answer just say it directly.
 
     KNOWLEDGE BASE:
     {' '.join(knowledge_snippets)}
