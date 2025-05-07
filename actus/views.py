6c68dfdcd0efa72750431a7017b15b49f4393126
3rwinn <ismaelgrebo@gmail.com>
2025-05-07 16:16:06 +0000

    fix: deactivate unused api method for actu

--- actus/views.py
+++ actus/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .models import Actu
 from .serializers import ActuSerializer
 
-class ActuViewSet(viewsets.ModelViewSet):
+class ActuViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Actu.objects.all().order_by('-created_at')
-    serializer_class = ActuSerializer 
+    serializer_class = ActuSerializer
