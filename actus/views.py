445095c248ab9c176241467368e3a56e211dda44
3rwinn <ismaelgrebo@gmail.com>
2025-05-07 21:36:34 +0000

    fix stuff actu

--- actus/views.py
+++ actus/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from .models import Actu
 from .serializers import ActuSerializer
 
 class ActuViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Actu.objects.all().order_by('-created_at')
+    permission_classes = [AllowAny]
     serializer_class = ActuSerializer
